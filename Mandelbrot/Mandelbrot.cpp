38e4a44ad2492ec3b6508c3affe773a2d0ed264c
Mateus Orsoni <matorsoni45@gmail.com>
2021-07-03 22:31:08 -0300

    define image frame from region of complex plane

--- Mandelbrot/Mandelbrot.cpp
+++ Mandelbrot/Mandelbrot.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 
+// Dump RGB buffer as PPM image.
 void dumpAsPpm(uint8_t* buffer, int width, int height, const std::string& filename)
 {
     std::ofstream ofs;
@@ -48,15 +49,17 @@ double radius2(const Complex& z)
     return x*x + y*y;
 }
 
-void getColor(const Complex& z, uint8_t& r, uint8_t& g, uint8_t& b)
+void getColor(const Complex& c, uint8_t& r, uint8_t& g, uint8_t& b)
 {
     const double r0 = 0.0, r1 = 1.0;
     const double g0 = 0.0, g1 = 1.0;
     const double b0 = 0.5, b1 = 1.0;
     const int MAX_ITER = 200;
-    Complex z_n = z;
+    Complex z_n = c;
+    // Compute Mandelbrot iteration.
     for (int i = 0; i < MAX_ITER; ++i) {
-        z_n = square(z_n) + z;
+        z_n = square(z_n) + c;
+        // If the radius ever reaches 2, the point does not belong to the set.
         if (radius2(z_n) > 4) {
             const double alpha = static_cast<double>(i) / MAX_ITER;
             r = static_cast<uint8_t>(255 * (r0 * (1.0 - alpha) + r1 * alpha));
@@ -74,24 +77,24 @@ void getColor(const Complex& z, uint8_t& r, uint8_t& g, uint8_t& b)
 // Main -------------------------------------
 int main()
 {
-    // Create image frame buffer.
-    const int width = 3840;
-    const int height = 3000;
-    uint8_t* frame_buffer = new uint8_t[width * height * 3];
-
     // Define rectangle on the complex plane which will be used to create the image.
-    double aspect_ratio = static_cast<double>(height) / width;
-    double rect_width = 3.0;
-    double rect_height = aspect_ratio * rect_width;
     const Complex center = {-0.6, 0.0};
+    double rect_width = 3.0;
+    double rect_height = 2.35;
     Complex top_left = center + Complex{-rect_width / 2, rect_height / 2};
+
+    // Create image frame buffer.
+    const int pixel_density = 1000;
+    const int width = static_cast<int>(rect_width * pixel_density);
+    const int height = static_cast<int>(rect_height * pixel_density);
+    uint8_t* frame_buffer = new uint8_t[width * height * 3];
 
     // Loop through each pixel and get the color of the corresponding point on the complex plane.
     auto ptr = frame_buffer;
-    double dx = rect_width / width;
-    double dy = -rect_height / height;
+    const double dx = rect_width / width;
+    const double dy = -rect_height / height;
     Complex z = top_left;
-    uint8_t r = 255, g = 0, b = 255;
+    uint8_t r, g, b;
     for (int row = 0; row < height; ++row) {
         for (int col = 0; col < width; ++col) {
             getColor(z, r, g, b);
@@ -106,7 +109,7 @@ int main()
         z = top_left;
     }
 
-    dumpAsPpm(frame_buffer, width, height, "test.ppm");
+    dumpAsPpm(frame_buffer, width, height, "Mandelbrot.ppm");
 
     delete[] frame_buffer;
     return 0;
